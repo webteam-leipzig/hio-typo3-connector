66d99c01770ce3eb6ea3ad60fb9b7114ab9d3da3
janbager <jan.bager@gmail.com>
2025-09-01 13:56:39 +0200

    feat: add event and listener for attaching HIO organization units to persons

--- Classes/EventListener/ReceiveHioOrgUnitListener.php
+++ Classes/EventListener/ReceiveHioOrgUnitListener.php
@@ -6,6 +6,7 @@ namespace Wtl\HioTypo3Connector\EventListener;
 
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Wtl\HioTypo3Connector\Domain\Repository\OrgUnitRepository;
+// use Wtl\HioTypo3Connector\Event\AttachHioOrgUnitToHioPersonsEvent;
 use Wtl\HioTypo3Connector\Event\ReceiveHioOrgUnitEvent;
 
 class ReceiveHioOrgUnitListener
@@ -20,5 +21,19 @@ class ReceiveHioOrgUnitListener
     public function __invoke(ReceiveHioOrgUnitEvent $event): void
     {
         $this->repository->save($event->getHioOrgUnit(), $event->getStoragePid());
+        $hioOrgUnitObjectId = $event->getHioOrgUnit()->getObjectId();
+        /**
+         todo: get persons from org unit
+        $hioPersonObjectIds = array_map(
+            static fn($hioPerson) => $hioPerson->getId(),
+            $event->getHioOrgUnit()->getPersons() ?? []
+        );
+        $this->eventDispatcher->dispatch(
+            new AttachHioOrgUnitToHioPersonsEvent(
+                $hioOrgUnitObjectId,
+                $hioPersonObjectIds
+            )
+        );
+        */
     }
 }
