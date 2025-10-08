6f73bfecd181e8c0538f8f2c5fb6f1de3af7c922
janbager <jan.bager@gmail.com>
2025-10-08 14:47:48 +0200

    feat: refactor affiliation handling to support multiple affiliations and add validity traits

--- Classes/Domain/Dto/Person/OrgUnitDto.php
+++ Classes/Domain/Dto/Person/OrgUnitDto.php
@@ -5,17 +5,15 @@ namespace Wtl\HioTypo3Connector\Domain\Dto\Person;
 
 class OrgUnitDto
 {
-    protected string $affiliation = '';
+    protected ?array $affiliations = [];
     protected int $id;
     protected string $name = '';
-    protected ?\DateTime $validFrom = null;
-    protected ?\DateTime $validTo = null;
-    protected string $visibility = '';
 
     public function getId(): int
     {
         return $this->id;
     }
+
     public function setId(int $id): void
     {
         $this->id = $id;
@@ -25,45 +23,20 @@ class OrgUnitDto
     {
         return $this->name;
     }
+
     public function setName(string $name): void
     {
         $this->name = $name;
     }
 
-    public function getAffiliation(): string
+    public function getAffiliations(): ?array
     {
-        return $this->affiliation;
-    }
-    public function setAffiliation(string $affiliation): void
-    {
-        $this->affiliation = $affiliation;
+        return $this->affiliations;
     }
 
-    public function getValidFrom(): ?\DateTime
+    public function setAffiliations(?array $affiliations): void
     {
-        return $this->validFrom;
-    }
-    public function setValidFrom(?\DateTime $validFrom): void
-    {
-        $this->validFrom = $validFrom;
-    }
-
-    public function getValidTo(): ?\DateTime
-    {
-        return $this->validTo;
-    }
-    public function setValidTo(?\DateTime $validTo): void
-    {
-        $this->validTo = $validTo;
-    }
-
-    public function getVisibility(): string
-    {
-        return $this->visibility;
-    }
-    public function setVisibility(string $visibility): void
-    {
-        $this->visibility = $visibility;
+        $this->affiliations = $affiliations;
     }
 
     static public function fromArray(array $data): self
@@ -71,13 +44,11 @@ class OrgUnitDto
         if (count($data) === 0) {
             return new self();
         }
+
         $dto = new self();
         $dto->setId($data['id']);
         $dto->setName($data['name'] ?? '');
-        $dto->setAffiliation($data['affiliation'] ?? '');
-        $dto->setValidFrom(isset($data['validFrom']) ? new \DateTime($data['validFrom']) : null);
-        $dto->setValidTo(isset($data['validTo']) ? new \DateTime($data['validTo']) : null);
-        $dto->setVisibility($data['visibility'] ?? '');
+        $dto->setAffiliations($data['affiliations'] ?? []);
 
         return $dto;
     }
