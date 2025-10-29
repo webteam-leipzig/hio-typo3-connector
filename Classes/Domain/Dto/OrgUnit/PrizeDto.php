49ecd4f9cc3ae8b7001bb317529c86e397267717
Jan Bager <jan.bager@gmail.com>
2025-10-29 10:33:18 +0100

    feat: introduce EndowedDto, ScopeDto, and TypeDto (#27)
    * feat: introduce EndowedDto, ScopeDto, and TypeDto for enhanced nomination handling
    
    * feat: refactor budget source type extraction in ProjectRepository

--- Classes/Domain/Dto/OrgUnit/PrizeDto.php
+++ Classes/Domain/Dto/OrgUnit/PrizeDto.php
@@ -2,7 +2,9 @@
 
 namespace Wtl\HioTypo3Connector\Domain\Dto\OrgUnit;
 
+use Wtl\HioTypo3Connector\Domain\Dto\Misc\EndowedDto;
 use Wtl\HioTypo3Connector\Domain\Dto\Misc\StatusDto;
+use Wtl\HioTypo3Connector\Domain\Dto\Misc\TypeDto;
 use Wtl\HioTypo3Connector\Domain\Dto\Nomination\OrgUnitDto;
 use Wtl\HioTypo3Connector\Trait\WithDescription;
 use Wtl\HioTypo3Connector\Trait\WithId;
@@ -16,11 +18,12 @@ class PrizeDto
     use WithId;
     use WithStatus;
     use WithTitle;
-    use WithType;
 
     protected ?array $orgUnits = null;
     protected ?string $category;
-    protected ?string $endowed;
+    protected ?EndowedDto $endowed;
+
+    protected ?TypeDto $type;
 
     public function getOrgUnits(): ?array
     {
@@ -39,13 +42,23 @@ class PrizeDto
     {
         $this->category = $category;
     }
-    public function getEndowed(): ?string
+
+    public function getEndowed(): ?EndowedDto
     {
         return $this->endowed;
     }
-    public function setEndowed(?string $endowed): void
+    public function setEndowed(?EndowedDto $endowed): void
     {
         $this->endowed = $endowed;
+    }
+
+    public function getType(): ?TypeDto
+    {
+        return $this->type;
+    }
+    public function setType(?TypeDto $type): void
+    {
+        $this->type = $type;
     }
 
     static function fromArray(array $data): self
@@ -53,12 +66,12 @@ class PrizeDto
         $instance = new self();
         $instance->setCategory($data['category'] ?? null);
         $instance->setDescription($data['description'] ?? null);
-        $instance->setEndowed($data['endowed'] ?? null);
+        $instance->setEndowed(EndowedDto::fromArray($data['endowed']) ?? null);
         $instance->setId($data['id'] ?? null);
         $instance->setOrgUnits(array_map(fn($item) => OrgUnitDto::fromArray($item), $data['orgUnits'] ?? []));
         $instance->setStatus(StatusDto::fromArray($data['status']) ?? null);
         $instance->setTitle($data['title'] ?? null);
-        $instance->setType($data['type'] ?? null);
+        $instance->setType(TypeDto::fromArray($data['type']) ?? null);
         return $instance;
     }
 }
