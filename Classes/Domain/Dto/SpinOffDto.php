39fee0d199dbfaba222801ebac78e3439c3a3553
janbager <jan.bager@gmail.com>
2025-11-25 11:00:39 +0100

    feat: extend SpinOffDto with new ResearchAreaDto, ResearchAreaKdsfDto, and SubjectAreaDto integration

--- Classes/Domain/Dto/SpinOffDto.php
+++ Classes/Domain/Dto/SpinOffDto.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace Wtl\HioTypo3Connector\Domain\Dto;
 
 use Wtl\HioTypo3Connector\Domain\Dto\Misc\LanguageDto;
+use Wtl\HioTypo3Connector\Domain\Dto\Misc\ResearchAreaDto;
+use Wtl\HioTypo3Connector\Domain\Dto\Misc\ResearchAreaKdsfDto;
+use Wtl\HioTypo3Connector\Domain\Dto\Misc\SubjectAreaDto;
 use Wtl\HioTypo3Connector\Domain\Dto\SpinOff\OrgUnitDto;
 use Wtl\HioTypo3Connector\Trait\WithDescription;
 use Wtl\HioTypo3Connector\Trait\WithDetails;
@@ -16,18 +19,28 @@ use Wtl\HioTypo3Connector\Trait\WithStartDate;
 
 class SpinOffDto
 {
-    use WithName;
-    use WithObjectId;
-    use WithDetails;
-    use WithSearchIndex;
     use WithDescription;
+    use WithDetails;
     use WithEndDate;
     use WithLanguage;
+    use WithName;
+    use WithObjectId;
+    use WithSearchIndex;
     use WithStartDate;
 
     // @var OrgUnitDto[]
     protected array $orgUnits = [];
+    /**
+     * @var ResearchAreaDto[]
+     */
     protected array $researchAreas = [];
+    /**
+     * @var ResearchAreaKdsfDto[]
+     */
+    protected array $researchAreasKdsf = [];
+    /**
+     * @var SubjectAreaDto
+     */
     protected array $subjectAreas = [];
 
     public function getOrgUnits(): array
@@ -50,6 +63,14 @@ class SpinOffDto
         $this->researchAreas = $researchAreas;
     }
 
+    public function getResearchAreasKdsf(): array
+    {
+        return $this->researchAreasKdsf;
+    }
+    public function setResearchAreasKdsf(array $researchAreasKdsf): void
+    {
+        $this->researchAreasKdsf = $researchAreasKdsf;
+    }
     public function getSubjectAreas(): array
     {
         return $this->subjectAreas;
@@ -71,13 +92,13 @@ class SpinOffDto
         $dto->setEndDate(isset($data['endDate']) ? new \DateTime($data['endDate']) : null);
         $dto->setLanguage(LanguageDto::fromArray($data['language']) ?? null);
         $dto->setName($data['name'] ?? '');
-        $dto->setStartDate(isset($data['startDate']) ? new \DateTime($data['startDate']) : null);
 
-        $orgUnits = [];
-        foreach ($data['orgUnits'] ?? [] as $orgUnit) {
-            $orgUnits[] = OrgUnitDto::fromArray($orgUnit);
-        }
-        $dto->setOrgUnits($orgUnits);
+        $dto->setOrgUnits(array_map(fn($item) => OrgUnitDto::fromArray($item), $data['orgUnits'] ?? []));
+        $dto->setResearchAreas(array_map(fn($item) => ResearchAreaDto::fromArray($item), $data['researchAreas'] ?? []));
+        $dto->setResearchAreasKdsf(array_map(fn($item) => ResearchAreaKdsfDto::fromArray($item), $data['researchAreasKdfs'] ?? []));
+        $dto->setSubjectAreas(array_map(fn($item) => SubjectAreaDto::fromArray($item), $data['subjectAreas'] ?? []));
+
+        $dto->setStartDate(isset($data['startDate']) ? new \DateTime($data['startDate']) : null);
 
         return $dto;
     }
